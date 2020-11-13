c753b941cfb785fb19bd65af12e9756966da070a
lecocopuff <74345825+lecocopuff@users.noreply.github.com>
2020-11-13 13:22:12 -0500

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -6,13 +6,16 @@ def main():
   min = 1
   max = 6
   
+  dice1 = random.randint(min,max)
+  dice2 = random.randint(min,max)
+  
   roll_again = 'yes'
   
   while roll_again == "yes" or roll_again == "y":
     print('Rolling the dices...')
     print('Its a winner....')
-    print random.randint(min,max)
-    print random.randint(min,max)
+    print(dice1)
+    print(dice2)
   
   roll again = raw_input("Roll the dices again?")
 
